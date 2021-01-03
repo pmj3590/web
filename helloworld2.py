bf8317f3f9f6de57661c43f9fad11e9c307353e7
pmj3590 <76635951+pmj3590@users.noreply.github.com>
2021-01-03 09:07:39 +0900

    Add files via upload

---
+++ helloworld2.py
@@ -0,0 +1,5 @@
+#!‪C:\Users\pmj35\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\python.exe
+a = 3+4+5
+b = a/3
+print("content-type:text/html; charset=UTF-8\n")
+print(b)
